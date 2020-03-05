50055feecf452426cc9209dd9f05f484a61154d7
MalCaor <xav.lemen@orange.fr>
2020-03-05 22:06:22 +0100

    Array of feed

--- Computer.py
+++ Computer.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 
 import json
 
+import feedparser
+
 # read file
 with open('config.json', 'r') as myfile:
     data=myfile.read()
@@ -19,7 +21,18 @@ token = conf['token']
 
 client = discord.Client()
 
+# rss feed
+rssTest = "http://www.reddit.com/r/python/.rss"
+Allrss = ["http://www.reddit.com/r/python/.rss",
+    "https://www.reddit.com/r/programming/.rss",
+    "https://www.reddit.com/r/AskProgramming/.rss"]
 
+feed = feedparser.parse(rssTest)
+Allfeed = []
+for rss in Allrss:
+    Allfeed.append(feedparser.parse(rss))
+
+# message event
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
@@ -28,6 +41,38 @@ async def on_message(message):
 
     if message.content.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
+        await message.channel.send(msg)
+
+    # Name Feed Test
+    if message.content.startswith('!feedtestNAME'):
+        msg = feed['feed']['title']
+        await message.channel.send(msg)
+
+    # Desc Feed Test
+    if message.content.startswith('!feedtestDESC'):
+        msg = feed.feed.subtitle
+        await message.channel.send(msg)
+
+    # Link Feed Test
+    if message.content.startswith('!feedtestLINK'):
+        msg = feed['feed']['link']
+        await message.channel.send(msg)
+
+    # Array Feed Top
+    if message.content.startswith('!feedTop'):
+        y = 3
+        if message.content.replace('!feedTop', ''):
+            num = message.content.replace('!feedTop ', '')
+            if isinstance(int(num), int):
+                y = int(num)
+
+        if y > len(Allrss):
+            msg = "Too big index, the current array lenght is " + str(len(Allrss))
+        else:
+            msg = ""
+            for x in range(len(Allrss) - (len(Allrss)-y)):
+                msg = msg + Allfeed[x]['feed']['title'] + "\n"
+
         await message.channel.send(msg)
 
 client.run(token)
