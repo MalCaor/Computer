40c402d5f94d2f11687d0d749f5ee42734ab985a
MalCaor <xav.lemen@orange.fr>
2020-04-01 14:39:34 +0200

    caps are not important anymore

--- Computer.py
+++ Computer.py
@@ -40,37 +40,39 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    mess = message.content.lower()
+
+    if mess.startswith('!hello'):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
 
-    if message.content.startswith('!num'):
+    if mess.startswith('!num'):
         msg = "false"
-        if any(char.isdigit() for char in message.content):
+        if any(char.isdigit() for char in mess):
             msg = "true"
         await message.channel.send(msg)
 
-    if message.content.startswith('!git'):
+    if mess.startswith('!git'):
         msg = "Here is the github repo " + repoGit
         await message.channel.send(msg)
 
     # Name Feed Test
-    if message.content.startswith('!feedtestNAME'):
+    if mess.startswith('!feedtestname'):
         msg = feed['feed']['title']
         await message.channel.send(msg)
 
     # Desc Feed Test
-    if message.content.startswith('!feedtestDESC'):
+    if mess.startswith('!feedtestdesc'):
         msg = feed.feed.subtitle
         await message.channel.send(msg)
 
     # Link Feed Test
-    if message.content.startswith('!feedtestLINK'):
+    if mess.startswith('!feedtestlink'):
         msg = feed['feed']['link']
         await message.channel.send(msg)
 
-    if message.content.startswith('!feedAdd'):
-        newfeedstr = message.content.replace('!feedAdd ', '')
+    if mess.startswith('!feedadd'):
+        newfeedstr = mess.replace('!feedadd ', '')
         newfeed = feedparser.parse(newfeedstr)
         if('title' in newfeed.feed):
             Allfeed.append(newfeed)
@@ -80,25 +82,25 @@ async def on_message(message):
         await message.channel.send(msg)
 
     # Array Feed Top
-    if message.content.startswith('!feedTop'):
+    if mess.startswith('!feedtop'):
         y = len(Allfeed)
-        if any(char.isdigit() for char in message.content):
+        if any(char.isdigit() for char in mess):
             y = ""
-            for char in message.content:
+            for char in mess:
                 if char.isdigit():
                     y = y + char
             y = int(y)
         if y > len(Allfeed):
             msg = "num too big, the current lenght is " + len(Allfeed)
-        elif "Desc" in message.content :
+        elif "desc" in mess :
             msg = ""
             for x in range(len(Allfeed) - (len(Allfeed)-y)):
                 msg = msg + "* "+Allfeed[x]['feed']['subtitle'] + "\n"
-        elif "Name" in message.content :
+        elif "name" in mess :
             msg = ""
             for x in range(len(Allfeed) - (len(Allfeed)-y)):
                 msg = msg + "* "+Allfeed[x]['feed']['title'] + "\n"
-        elif "Link" in message.content :
+        elif "link" in mess :
             msg = ""
             for x in range(len(Allfeed) - (len(Allfeed)-y)):
                 msg = msg + "* "+Allfeed[x]['feed']['link'] + "\n"
