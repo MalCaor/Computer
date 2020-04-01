3e27fc98e0d95a246f046f0a17d832dda0e1cb93
MalCaor <xav.lemen@orange.fr>
2020-04-01 15:43:10 +0200

    not finish ascyn

--- Computer.py
+++ Computer.py
@@ -9,6 +9,10 @@ import json
 
 import feedparser
 
+import time
+
+import asyncio
+
 # read file
 with open('config.json', 'r') as myfile:
     data=myfile.read()
@@ -25,6 +29,12 @@ client = discord.Client()
 rssTest = "http://www.reddit.com/r/python/.rss"
 Allrss = conf['Allrss']
 
+# list of post already posted
+usedpost = []
+
+# the chan where the post must print
+chan = None
+
 # git depot
 repoGit = "https://github.com/MalCaor/Computer"
 
@@ -32,6 +42,8 @@ feed = feedparser.parse(rssTest)
 Allfeed = []
 for rss in Allrss:
     Allfeed.append(feedparser.parse(rss['url']))
+
+update = True
 
 # message event
 @client.event
@@ -46,10 +58,25 @@ async def on_message(message):
         msg = 'Hello {0.author.mention}'.format(message)
         await message.channel.send(msg)
 
+    if mess.startswith('!posthere'):
+        chan = message.channel
+        msg = 'ok, i will post here'
+        await message.channel.send(msg)
+
     if mess.startswith('!num'):
         msg = "false"
         if any(char.isdigit() for char in mess):
             msg = "true"
+        await message.channel.send(msg)
+
+    if mess.startswith('!stopupdate'):
+        update = False
+        msg = 'update have been stop'
+        await message.channel.send(msg)
+
+    if mess.startswith('!startupdate'):
+        update = True
+        msg = 'update starting'
         await message.channel.send(msg)
 
     if mess.startswith('!git'):
@@ -108,5 +135,25 @@ async def on_message(message):
         #msg = str(y)
         await message.channel.send(msg)
 
+
+# UPDATE
+async def updatePost():
+    while update:
+        msg = ''
+        for feed in Allfeed:
+            i = 0
+            for post in feed.entries:
+                if post not in usedpost:
+                    # print the post
+                    msg = msg + post.title
+                    if chan != None:
+                        chan.send(msg)
+                    # only 5 post per time
+                    i = i + 1
+                    if(i>5):
+                        break
+
+loop.create_task()
+loop.run_until_complete(updatePost())
 
 client.run(token)
